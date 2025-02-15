1f2ff8a538ca643d3153ffb52f3a910da4fe3bbf
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-02-15 16:06:34 +0900

    fix: 未使用import文をコメントアウト

--- components/ui/animated-hero.tsx
+++ components/ui/animated-hero.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import { motion } from 'framer-motion'
-import { MoveRight, PhoneCall } from 'lucide-react'
+// import { MoveRight, PhoneCall } from 'lucide-react'
+import { MoveRight } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
